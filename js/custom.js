542782e4e1da15b5630c23d4bc1d91d56ddb5df7
KhairulIslamAzam <khairulislam.azam@gmail.com>
2019-06-17 15:06:26 +0600

    Full font end design of website

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+// bxslider
+
+// Add bx slider to screens
+$(document).ready(function() {
+    
+    $('.bxslider').bxSlider({
+        
+        slideWidth: 292.5,
+        auto: true,
+        minSlides: 1, 
+        maxSlides: 9, 
+        slideMargin: 50
+    });
+    
+});
